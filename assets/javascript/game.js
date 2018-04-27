27241d5345b92c28b9543ac158bcc9acee2963d7
Kennan <kennan.smith1@gmail.com>
2018-04-26 22:02:20 -0600

    my brain is too melted to even evaluate what i changed, but i think i just added the pic properties

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,51 @@
-// VARIABLES
-
 var starWarsRPG = {
     characters: {
         hanSolo: {
-            stats = [
+            stats: [
                 hp = 200,
                 ap = 5,
                 cap = 10
-            ]
+            ],
+            pic: "../../assets/images/vaderThumbsUp.jpg",
         },
         luke: {
-            stats = [
+            stats: [
                 hp = 125,
                 ap = 7,
                 cap = 8
-            ]
+            ],
+            pic: "../../assets/images/vaderThumbsUp.jpg",
         },
         vader: {
-            stats = [
+            stats: [
                 hp = 250,
                 ap = 3,
                 cap = 8
-            ]
+            ],
+            pic: "../../assets/images/vaderThumbsUp.jpg",
         },
         emperor: {
-            stats = [
+            stats: [
                 hp = 100,
                 ap = 9,
                 cap = 12
-            ]
+            ],
+            pic: "../../assets/images/vaderThumbsUp.jpg",
         },
         userCharacter: {
-            stats = [
+            stats: [
                 hp = 1,
                 ap = 1,
                 cap = 1
-            ]
+            ],
+            pic: "../../assets/images/vaderThumbsUp.jpg",
         },
     },
 
     functions: {
+        newGame: function() {
+
+        },
         chooseCharacter: function() {
 
         },
@@ -47,7 +53,7 @@ var starWarsRPG = {
 
         },
         attack: function() {
-            
+
         }
     },
 }
