c5eb2960c7e995ec1855a6d12fff7c2b87867447
Kennan <kennan.smith1@gmail.com>
2018-04-26 19:03:24 -0600

    started building game object in game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,42 @@
 // VARIABLES
 
+var starWarsRPG = {
+    characters: {
+        hanSolo: {
+            stats = [
+                hp = 200,
+                ap = 5,
+                cap = 10
+            ]
+        },
+        luke: {
+            stats = [
+                hp = 125,
+                ap = 7,
+                cap = 8
+            ]
+        },
+        vader: {
+            stats = [
+                hp = 250,
+                ap = 3,
+                cap = 8
+            ]
+        },
+        emperor: {
+            stats = [
+                hp = 100,
+                ap = 9,
+                cap = 12
+            ]
+        },
+        userCharacter: {
+            stats = [
+                hp = 1,
+                ap = 1,
+                cap = 1
+            ]
+        },
+    },
+    
+}
